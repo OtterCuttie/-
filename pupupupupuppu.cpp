c9506e7e4ef5cd7b9503a403c870ce7358a73b40
OtterCuttie <144522961+OtterCuttie@users.noreply.github.com>
2023-10-25 15:56:29 +1000

    Update pupupupupuppu.cpp

--- pupupupupuppu.cpp
+++ pupupupupuppu.cpp
@@ -1,54 +1,55 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	int mass[2];
+int main() {
+	int n, m=0;
+	cin >> n, m;
+	int** mass = new int* [n];
+
 	int sum = 0;
 	int massLength = sizeof(mass) / sizeof(int);
-
-
-
-	for (int i = 0; i < massLength; i++) {
-		mass[i] = i +1;
+	
+    for (int i = 0; i < n; i++) {
+		mass[i] = new int[m];
 	}
-
-	bool brk = true;
-	int gogo=0;
-	for (int i = 0; i < massLength; i++) {
-		sum += mass[i];
-	}
-	for (int i = 2; i <= round(sqrt(sum)); i++) {
-		if (sum % i == 0) {
-			cout << "sum of number is compound ";
-			brk = false;
-			int count = 0;
-			for (int r = 0; r < massLength; r++) {
-				cout << r;
-				count++;
-			}
-			break;
-		}
-
-	}
-
-	if (brk) {
+	for (int j = 0; j < n;j++) {
+		bool brk = true;
+		int gogo = 0;
 		for (int i = 0; i < massLength; i++) {
-			cout << "sum of number is NOT compound";
-			if (mass[i] % 2 != 0 and mass[i]!=2) {
-				cout << sum - mass[i] << endl << massLength-1<<endl;
-				gogo=mass[i];
-				for(int j = 0; j < massLength; j++)
-					if (mass[j] != gogo) {
-						cout << j << " ";
-						break;
-					}
+			sum += mass[j][i];
+		}
+		for (int i = 2; i <= round(sqrt(sum)); i++) {
+			if (sum % i == 0) {
+				cout << "sum of number is compound ";
+				brk = false;
+				int count = 0;
+				for (int r = 0; r < massLength; r++) {
+					cout << r;
+					count++;
+				}
 				break;
 			}
+
 		}
+
+		if (brk) {
+			for (int i = 0; i < massLength; i++) {
+				cout << "sum of number is NOT compound";
+				if (mass[j][i] % 2 != 0 and mass[j][i] != 2) {
+					cout << /*sum - mass[i] << endl << */ massLength - 1 << endl;
+					gogo = mass[j][i];
+					for (int k = 0; k < massLength; k++) {
+						if (mass[j][k] != gogo) {
+							cout << j << " ";
+							break;
+						}
+						break;
+					}
+				}
+			}
+		}
+
+		sum = 0;
 	}
-
-	sum = 0;
-
 	return 0;
-
+}
