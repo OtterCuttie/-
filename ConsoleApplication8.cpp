ededc95c74eec7e6a91df34ac0eeb63a68362e89
OtterCuttie <144522961+OtterCuttie@users.noreply.github.com>
2023-11-01 13:45:25 +1000

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,75 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0,"");
+	int n, m = 0;
+	cin >> n;
+	int** mass = new int* [n];
+
+	int sum = 0;
+	int massLength = 0;
+
+	for (int j = 0; j < n;j++) {
+
+		//вводим количество элементов и создаем, рассчитываем длину массива
+	    cin >> m;
+		mass[j] = new int[m];
+		massLength = sizeof(mass[j]) / sizeof(int);
+
+		//вводим элементы
+		for (int i = 0; i < m; i++) {
+			cin >> mass[j][i];
+		}
+
+		bool brk = true;
+		int gogo = 0;
+		
+		//танцы с бубнами вокруг суммы
+		//складываем
+		for (int i = 0; i < massLength; i++) {
+			sum += mass[j][i];
+		}
+
+		for (int i = 2; i <= round(sqrt(sum))+1; i++) {
+		//если число суммы составное
+			if (sum % i == 0) {
+				cout << "sum of number is compound " << endl;;
+				brk = false;
+				int count = 0;
+				cout << "Количество элементов:" << massLength << endl;
+				cout << "Индексы элементов:";
+				for (int r = 0; r < massLength; r++) {
+					cout << r + 1 << " ";
+					count++;
+				}
+				break;
+			}
+
+		}
+		//если не составное
+		if (brk==true) {
+			for (int i = 0; i < massLength; i++) {
+				cout << "sum of number is NOT compound"<<endl;
+				if (mass[j][i] % 2 != 0 and mass[j][i] != 2) {
+					//выводим длину подмножества
+					cout << /*sum - mass[i] << endl << */"Количество элементов:" << massLength - 1 << endl;
+					//первое попашееся нечетное число принмаем за гого и выводим индексы элементов подмножества
+					gogo = mass[j][i];
+					cout << "Индексы элементов:";
+					for (int k = 0; k < massLength; k++) {
+						if (mass[j][k] != gogo) {
+							cout <<  k+1 << " ";
+							break;
+						}
+						break;
+					}
+				}
+			}
+		}
+
+		sum = 0;
+	}
+	return 0;
+}
---
+++ worn tiles and roads.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+	int const X = 5;
+	int const Y = 4;
+	int mass[X][Y];
+	int n = 5;
+	//рандомно ломаем плитки
+	srand(time(nullptr));
+	for (int x = 0; x < X; x++){
+		for (int y = 0; y < Y; y++) {
+			int randomNum = rand() % 5 - 1;
+			if (randomNum == 1 or randomNum == 2) {
+				mass[x][y] = 1;
+			}
+			else {
+				mass[x][y] = 0;
+			}
+		}
+	}
+	//вывод массива
+	for (int x = 0; x < X; x++){
+		for (int y = 0; y < Y; y++) {
+			cout << mass[x][y] << " ";
+		}
+		cout << endl;
+	}
+	//нахождение расстояние между "сломаннными плитками" горизонтально и вертикально 
+	for (int x = 0; x < X; x++){
+		int start = 0;
+		int end = 0;
+		int k = 0;
+		bool isStart = false;
+		for (int y = 0; y < Y; y++) {
+			if (mass[x][y] == 1) {
+				k++;
+			}
+			//start 
+			if (mass[x][y] == 1 and isStart != true) {
+				start = y;
+				isStart = true;
+			}
+			//end
+			if (mass[x][y] == 1 and y > start) {
+				end = y;
+				break;
+			}
+		}
+		if (end - start > 0) {
+			cout << "Horizontally" << endl;
+			cout << "Start: " << start << " End: " << end << " Width: " << end - start + 1 << endl;
+		}
+		//если у нас больше чем две сломанных плиток 
+		if (k>2) {
+			//повторяем столько раз сколько количество сломманых плиток больше чем два ( 3 сломманых плитки 3-2=1 повтрояется один раз) 
+			for (int h = 0; h < k - 2; h++) {
+				start == end;
+				//находим новый end
+				for (int g = end + 1; g < Y; g++) {
+					if (mass[x][g] == 1 and g > start) {
+						end = g;
+						break;
+					}
+				}
+				if (end - start > 0) {
+					cout << "Horizontally" << endl;
+					cout << "Start: " << start << " End: " << end << " Width: " << end - start + 1 << endl;
+				}
+			}
+			//задумка была в том что мы находим минимальную ширину между всеми сломанными плитками, 
+			// но как мы будем запоминать значения и сравнивать их между собой? 
+			// У нас нет определеного количества width чтобы каждый раз создавать переменные 
+		}
+	}
+}
